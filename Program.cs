6971bdf4db32c5d49ca9d5eac366717f0f8444c1
Vincent Thevenon <24572@student.dorset.college.ie>
2021-11-03 16:57:25 +0000

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExceptionDemo001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] numbers = new int[2];
+
+            try
+            {
+                numbers[0] = 0;
+                numbers[1] = 1;
+                numbers[2] = 2;
+
+                foreach(int i in numbers)
+                { 
+                    Console.WriteLine(i);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("An exception was thrown");
+            }
+        }
+    }
+}
